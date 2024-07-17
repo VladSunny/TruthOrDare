2f6b5194ab4d6e073b460e0ac19e85fa9f7db15d
Vlad Sunny <vladislavsunny@gmail.com>
2024-07-17 08:56:49 +0300

    Material UI setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center'>
       <h1>Hello, world!</h1>
+      <Button variant="contained">Contained</Button>
     </div>
   );
 }
